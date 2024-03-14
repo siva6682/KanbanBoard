054f9abe5ff151c3d8c7728b832141b0c2c97fe5
siva6682 <javvadisivakumar6682@gmail.com>
2024-03-14 14:36:18 +0530

    sixth commit adding the checklist file

---
+++ src/main/java/com/kanban/entity/Checklist.java
@@ -0,0 +1,28 @@
+package com.kanban.entity;
+
+public class Checklist {
+
+    private String name;
+    private String location;
+
+    public Checklist(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
