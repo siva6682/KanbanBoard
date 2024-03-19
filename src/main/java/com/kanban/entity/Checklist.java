f1ab26868ccdf6c28835cb9805afb89f60397c32
siva6682 <javvadisivakumar6682@gmail.com>
2024-03-19 11:39:01 +0530

    changes according to the main

--- src/main/java/com/kanban/entity/Checklist.java
+++ src/main/java/com/kanban/entity/Checklist.java
@@ -2,6 +2,60 @@ package com.kanban.entity;
 
 public class Checklist {
 
-  
+    private String name;
+    private String location;
+
+    private int mobileNo;
+
+    private String Address;
+
+    public Checklist(String name, String location, int mobileNo, String address) {
+        this.name = name;
+        this.location = location;
+        this.mobileNo = mobileNo;
+        Address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(int mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Checklist{" +
+                "name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", mobileNo=" + mobileNo +
+                ", Address='" + Address + '\'' +
+                '}';
+    }
   
 }
